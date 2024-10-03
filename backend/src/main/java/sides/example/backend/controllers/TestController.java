0f9b3b260249df3cc848942cc6d13d3a8b89fdd8
BogdanEmilian <emilian.bogdan02@e-uvt.ro>
2024-10-03 17:19:37 +0300

    test controller

---
+++ backend/src/main/java/sides/example/backend/controllers/TestController.java
@@ -0,0 +1,13 @@
+package sides.example.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testEndpoint() {
+        return "The application is running!";
+    }
+}
